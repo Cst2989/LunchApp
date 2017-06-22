92b1b09f663d52856830cc7c580c1966d399572f
Neciu Dan <cst2989@gmail.com>
2017-06-22 12:44:19 +0300

    Added folder structure, styles and some componenents

---
+++ components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text
+} from 'react-native';
+
+
+class App extends Component {
+    render() {
+        return (
+            <Text>Hello World</Text>
+        );
+    }
+}
+
+export default App;
